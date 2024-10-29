9e46da3f66395bb25ed304ba17145497cb07ec54
The-R0nin <2025esteele@isd2144.org>
2024-10-29 12:12:15 -0500

    Update program_2.py
    Ethan Steele
    10/29
    Done?

--- program_2.py
+++ program_2.py
@@ -3,3 +3,13 @@
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+import random
+amount = 0
+rounds = int(input('number of rounds: '))
+with open('number.txt', 'w'):
+    while amount < rounds and rounds <= 1000:
+        with open('number.txt', 'a') as f:  
+            number = random.randint(0, 500)
+            lines = str(number) + '\n'
+            f.writelines(str(lines))        
+            amount = amount + 1
