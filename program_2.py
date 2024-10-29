aa57809feb6984fe7287ea9387a52acf378c59f6
The-R0nin <2025esteele@isd2144.org>
2024-10-29 12:36:15 -0500

    Update program_2.py
    Ethan Steele
    10/29
    Done? removed unnecessary code

--- program_2.py
+++ program_2.py
@@ -4,20 +4,12 @@
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
 import random
-def main():
-    try:
-        amount = 0
-        rounds = int(input('number of rounds: '))
-        with open('number.txt', 'w'):
-            while amount < rounds and rounds <= 1000 and rounds != 0:
-                with open('number.txt', 'a') as f:  
-                    number = random.randint(0, 500)
-                    lines = str(number) + '\n'
-                    f.writelines(str(lines))        
-                    amount = amount + 1
-    except ValueError:
-        print('number of rounds must be a whole number(intager)')
-        rounds = int(input('Please enter a non-sero number or type in 0 to exit:'))
-        if rounds != 0:
-            main()
-main()
+amount = 0
+rounds = int(input('number of rounds: '))
+with open('number.txt', 'w'):
+    while amount < rounds and rounds <= 1000 and rounds != 0:
+        with open('number.txt', 'a') as f:  
+            number = random.randint(0, 500)
+            lines = str(number) + '\n'
+            f.writelines(str(lines))        
+            amount = amount + 1
