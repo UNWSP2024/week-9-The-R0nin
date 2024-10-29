4b683fea6446c97be8038cd482d527097257fbef
The-R0nin <2025esteele@isd2144.org>
2024-10-29 13:02:58 -0500

    Update program_3.py
    Ethan Steele 
    10/29
    Done?

--- program_3.py
+++ program_3.py
@@ -10,7 +10,7 @@
 # are converted to a number.
 def sum_numbers_from_file():
     total = 0.0
-    
+
     try:
         numbers = open('numbers.txt', 'r')
         for line in numbers:
@@ -18,16 +18,16 @@ def sum_numbers_from_file():
             total = total + number
 
         numbers.close()
-
-        print('In the sum_numbers_from_file function')
-        print(total)
-
+        
     except IOError:
         print('error occurred while tring to read the file')
 
     except ValueError:
         print('non number value(data) found in the file')
 
+    finally:
+        print('In the sum_numbers_from_file function')
+        print(total)
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
